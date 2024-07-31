19df83fa3204fe29709a8931c2a9e15ed412b79e
BRUNO <112178886+BrunoGGL@users.noreply.github.com>
2024-07-31 09:08:24 -0300

    feitinho o ngc

--- script.js
+++ script.js
@@ -6,58 +6,108 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Você saiu para explorar uma reserva florestal e agora se encontra perdido na propria, sem nenhuma conexão de celular ou equipamentos necessário. Oque você faz? ",
         alternativas: [
-            "Isso é assustador!",
-            "Isso é maravilhoso!"
+            {
+                texto: "entro em desespero.",
+                afirmacao: "fiquei apenas parado esperando por ajuda."
+            },
+            {
+                texto: "procuro torre para me comunicar",
+                afirmacao: "sai andando procurando sinal."
+            }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "você verifica que tem uma bolsa, e encontra comida e uma bússula. Qual a sua escolha?",
         alternativas: [
-            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
+            {
+                texto: "Escolho a comida, pois estou com fome.",
+                afirmacao: "escolheu a comida para não morrer de fome."
+            },
+            {
+                texto: "Escolho a bússula para tentar me localizar na mata.",
+                afirmacao: "infelizmente a bússula nao ajudou, pois você não sabia usar.."
+            }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona",
+        enunciado: "você precisa procurar água entretanto você está perto de apenas um rio poluído .",
         alternativas: [
-            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+            {
+                texto: "você toma a água podre",
+                afirmacao: "você tomou a água contaminada e poderá ter complicações depois."
+            },
+            {
+                texto: "ainn que nojinho.",
+                afirmacao: "você não toma a água por estar contaminada, mas poderá ficar desidratado."
+            }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Ohh não, você se depara com um onça. E agora?",
         alternativas: [
-            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-            "Criar uma imagem utilizando um gerador de imagem de IA."
+            {
+                texto: "tento jogar um graveto para ela ir pegar.",
+                afirmacao: "você jogou o graveto para a onça ir buscar, mas ela ignora e te come."
+            },
+            {
+                texto: "subo em uma árvore, não sou loco",
+                afirmacao: "você consegue sobreviver ao ataque da onça subindo em uma árvore!"
+            }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "você acaba andando depois disso e encontra 2 casébres, um a direita e outro a esquerda . Qual você entra? ",
         alternativas: [
-            "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
+            {
+                texto: "entro no da direita.",
+                afirmacao: "você entra na casa da direita e Infelizmente ali morava dona ortensia ela jogou uma bola de basquete na sua cabeça e acabou te matando."
+            },
+            {
+                texto: "entro no da esquerda.",
+                afirmacao: "você entra na Casa da esquerda e la mora dona sheila, ela te ajuda chamando a defesa civil."
+            }
         ]
     },
 ];
 
+
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
-function mostraPergunta(){
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
-
 }
 
-function mostraalternativas(){
-    for(const alternativas of perguntaAtual.alternativas){
-       const botaoAlternativas = document.createElement("button");
-       botaoAlternativas.textContent = alternativas;
-       caixaAlternativas.appendChild(botaoAlternativas);
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Seu fim...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
